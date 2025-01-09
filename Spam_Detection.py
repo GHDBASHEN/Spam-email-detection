9a484943e15b4a7ed8f93de4cf69ddc829acb704
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-09 11:55:24 +0530

    Train the Model

--- Spam_Detection.py
+++ Spam_Detection.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 # Load the dataset
 data = pd.read_csv('spam.csv', encoding='latin-1')
@@ -20,3 +21,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 vectorizer = CountVectorizer()
 X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
+
+
+# Train a Naive Bayes model
+model = MultinomialNB()
+model.fit(X_train_vec, y_train)
